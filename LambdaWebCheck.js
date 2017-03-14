3b13c52eec141bc76297a450e899382d09a8df41
Brian Del Vecchio <bdv@hybernaut.com>
2017-03-14 12:09:53 -0400

    polling and catching timeouts

--- LambdaWebCheck.js
+++ LambdaWebCheck.js
@@ -8,20 +8,13 @@
 
 // external dependencies
 const AWS = require('aws-sdk');
-const request = require('request');
+const request = require('request-promise');
 const _ = require('underscore');
 
-// PUT YOUR URL HERE
-const url = 'https://mywebsite.com/';
 const timeout = 10000; // ms to wait for response
-
 
 // get reference to cloudwatch
 const cloudwatch = new AWS.CloudWatch();
-
-function matchesTag(item,key){
-
-}
 
 // discover the instances we should poll
 exports.discover = () => {
@@ -46,10 +39,9 @@ exports.discover = () => {
       data.Reservations.forEach(function(reservation) {
         const instance = reservation.Instances[0];
         const running = (instance.State.Name == 'running');
-        const instanceName = _.findWhere(instance.Tags, {Key: 'Name'}).Value;
 
         if (running) {
-          console.log(instanceName + ' is ' + instance.State.Name);
+          const instanceName = _.findWhere(instance.Tags, {Key: 'Name'}).Value;
           instances.push(instanceName);
         }
       });
@@ -64,48 +56,69 @@ exports.handler = (event, context, callback) => {
     // ignore invalid SSL certificate
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
+    this.discover().then(function(instances){
 
+      instances.forEach(function(instance){
+        const url = `https://${instance}.siteworx.io/api/v1/version`;
+        console.log(`checking ${url}`);
 
-    // call url and if response code is not 200 something is not working
-    request.get(url, {timeout: timeout},
-    (err, response, body) => {
+        request.get(url, {timeout: timeout})
+          .then(function(response, body){
 
-        let value = 0;
+            console.log(`${instance} response: ${response}`);
 
-        if (err) {
-            console.log('Error: ' + err);
-            value = 1;
-        }
-        else if (response.statusCode !== 200) {
-            console.log('Status Code: ' + response.statusCode);
-            value = 1;
-        }
-        else console.log('Status Code: 200');
+          })
+          .catch(function(reason){
+            console.log("request error: " + reason);
+          });
+      });
 
-        let params = {
-            MetricData: [ /* required */
-                {
-                    MetricName: 'WebSiteNotResponding', /* required */
-                    Dimensions: [
-                        {
-                            Name: 'url', /* required */
-                            Value: url /* required */
-                        }
-                    ],
-                    Timestamp: new Date(),
-                    Unit: 'Count',
-                    Value: value
-                }
-            ],
-            Namespace: 'ActValue' /* required */
-        };
-
-        cloudwatch.putMetricData(params, (err, data) => {
-            if (err) callback(err, 'KO');
-            else callback(null, data);
-        });
+    })
+    .catch(function(error){
+      console.log("discover error: " + error);
 
     });
+
+    // // call url and if response code is not 200 something is not working
+    // request.get(url, {timeout: timeout},
+    // (err, response, body) => {
+    //
+    //     let value = 0;
+    //
+    //     if (err) {
+    //         console.log('Error: ' + err);
+    //         value = 1;
+    //     }
+    //     else if (response.statusCode !== 200) {
+    //         console.log('Status Code: ' + response.statusCode);
+    //         value = 1;
+    //     }
+    //     else console.log('Status Code: 200');
+    //
+    //     let params = {
+    //         MetricData: [ /* required */
+    //             {
+    //                 MetricName: 'WebSiteNotResponding', /* required */
+    //                 Dimensions: [
+    //                     {
+    //                         Name: 'url', /* required */
+    //                         Value: url /* required */
+    //                     }
+    //                 ],
+    //                 Timestamp: new Date(),
+    //                 Unit: 'Count',
+    //                 Value: value
+    //             }
+    //         ],
+    //         Namespace: 'ActValue' /* required */
+    //     };
+    //
+    //     cloudwatch.putMetricData(params, (err, data) => {
+    //         if (err) callback(err, 'KO');
+    //         else callback(null, data);
+    //     });
+    //
+    // });
 
 
 };
